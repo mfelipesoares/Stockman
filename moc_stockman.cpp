6032444f2e95eb7e6086e2997050fecd1dfafcb0
Felipe Soares <mfelipesoares@outlook.com.br>
2023-08-18 12:04:09 -0300

    [NEW] - Projeto final

---
+++ moc_stockman.cpp
@@ -0,0 +1,219 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'stockman.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../Stockman/stockman.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'stockman.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSStockmanENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSStockmanENDCLASS = QtMocHelpers::stringData(
+    "Stockman",
+    "on_cadastrar_cliente_clicked",
+    "",
+    "on_cadastrar_produto_clicked",
+    "on_cadastrar_fornecedor_clicked",
+    "on_pesquisa_cliente_2_clicked",
+    "on_pushButton_clicked",
+    "on_pesquisa_produto_clicked",
+    "on_nova_consulta_produtos_clicked",
+    "on_pesquisa_fornecedor_clicked",
+    "on_nova_consulta_fornecedor_clicked"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSStockmanENDCLASS_t {
+    uint offsetsAndSizes[22];
+    char stringdata0[9];
+    char stringdata1[29];
+    char stringdata2[1];
+    char stringdata3[29];
+    char stringdata4[32];
+    char stringdata5[30];
+    char stringdata6[22];
+    char stringdata7[28];
+    char stringdata8[34];
+    char stringdata9[31];
+    char stringdata10[36];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSStockmanENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSStockmanENDCLASS_t qt_meta_stringdata_CLASSStockmanENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 8),  // "Stockman"
+        QT_MOC_LITERAL(9, 28),  // "on_cadastrar_cliente_clicked"
+        QT_MOC_LITERAL(38, 0),  // ""
+        QT_MOC_LITERAL(39, 28),  // "on_cadastrar_produto_clicked"
+        QT_MOC_LITERAL(68, 31),  // "on_cadastrar_fornecedor_clicked"
+        QT_MOC_LITERAL(100, 29),  // "on_pesquisa_cliente_2_clicked"
+        QT_MOC_LITERAL(130, 21),  // "on_pushButton_clicked"
+        QT_MOC_LITERAL(152, 27),  // "on_pesquisa_produto_clicked"
+        QT_MOC_LITERAL(180, 33),  // "on_nova_consulta_produtos_cli..."
+        QT_MOC_LITERAL(214, 30),  // "on_pesquisa_fornecedor_clicked"
+        QT_MOC_LITERAL(245, 35)   // "on_nova_consulta_fornecedor_c..."
+    },
+    "Stockman",
+    "on_cadastrar_cliente_clicked",
+    "",
+    "on_cadastrar_produto_clicked",
+    "on_cadastrar_fornecedor_clicked",
+    "on_pesquisa_cliente_2_clicked",
+    "on_pushButton_clicked",
+    "on_pesquisa_produto_clicked",
+    "on_nova_consulta_produtos_clicked",
+    "on_pesquisa_fornecedor_clicked",
+    "on_nova_consulta_fornecedor_clicked"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSStockmanENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       9,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   68,    2, 0x08,    1 /* Private */,
+       3,    0,   69,    2, 0x08,    2 /* Private */,
+       4,    0,   70,    2, 0x08,    3 /* Private */,
+       5,    0,   71,    2, 0x08,    4 /* Private */,
+       6,    0,   72,    2, 0x08,    5 /* Private */,
+       7,    0,   73,    2, 0x08,    6 /* Private */,
+       8,    0,   74,    2, 0x08,    7 /* Private */,
+       9,    0,   75,    2, 0x08,    8 /* Private */,
+      10,    0,   76,    2, 0x08,    9 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject Stockman::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSStockmanENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSStockmanENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSStockmanENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<Stockman, std::true_type>,
+        // method 'on_cadastrar_cliente_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_cadastrar_produto_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_cadastrar_fornecedor_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pesquisa_cliente_2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pesquisa_produto_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_nova_consulta_produtos_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pesquisa_fornecedor_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_nova_consulta_fornecedor_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void Stockman::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Stockman *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_cadastrar_cliente_clicked(); break;
+        case 1: _t->on_cadastrar_produto_clicked(); break;
+        case 2: _t->on_cadastrar_fornecedor_clicked(); break;
+        case 3: _t->on_pesquisa_cliente_2_clicked(); break;
+        case 4: _t->on_pushButton_clicked(); break;
+        case 5: _t->on_pesquisa_produto_clicked(); break;
+        case 6: _t->on_nova_consulta_produtos_clicked(); break;
+        case 7: _t->on_pesquisa_fornecedor_clicked(); break;
+        case 8: _t->on_nova_consulta_fornecedor_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *Stockman::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Stockman::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSStockmanENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int Stockman::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 9;
+    }
+    return _id;
+}
+QT_WARNING_POP
